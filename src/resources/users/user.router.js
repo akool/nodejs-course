778600a35e1a7556ec44548c247904f99b0e551e
akool <akoolny@gmail.com>
2021-05-15 22:08:32 +0300

    feat: implement routes for users

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,33 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
+
   res.json(users.map(User.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  const user = await usersService.get(req.params.id);
+
+  res.json(User.toResponse(user));
+});
+
+router.route('/').post(async (req, res) => {
+  const user = await usersService.post(new User(req.body));
+
+  res.status(201);
+  res.json(User.toResponse(user));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const user = await usersService.put(req.params.id, req.body);
+
+  res.json(User.toResponse(user));
+});
+
+router.route('/:id').delete(async (req, res) => {
+  await usersService.del(req.params.id, req.body);
+
+  res.sendStatus(204);
 });
 
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,9 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+const get = (id) => usersRepo.get(id);
+const post = (data) => usersRepo.post(data);
+const put = (id, data) => usersRepo.put(id, data);
+const del = (id) => usersRepo.del(id);
 
-module.exports = { getAll };
+module.exports = { getAll, get, post, put, del };
