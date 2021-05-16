7f5f62d4d0ace4088712c82275893ef90168b52d
akool <akoolny@gmail.com>
2021-05-16 20:27:02 +0300

    feat: add removing users from the tasks

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,9 +1,13 @@
 const usersRepo = require('./user.memory.repository');
+const { updateMany } = require('../tasks/task.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 const get = (id) => usersRepo.get(id);
 const post = (data) => usersRepo.post(data);
 const put = (id, data) => usersRepo.put(id, data);
-const del = (id) => usersRepo.del(id);
+const del = async (id) => {
+  await usersRepo.del(id);
+  await updateMany( { userId: id }, { userId: null });
+}
 
 module.exports = { getAll, get, post, put, del };
